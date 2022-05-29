4f10f30b5a64755a29bec96d971543f9597b9476
Ali Jawad <ali.jawad@rolustech.net>
2022-05-29 23:57:51 +0500

    Login and Logout APIs Implemented

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -20,6 +21,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(value) {
@@ -36,8 +38,36 @@ const userSchema = new mongoose.Schema({
             if (value.toLowerCase().includes('password'))
                 throw new Error("Password Can't have passwords");
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'IAmASoftwareEngineeer');
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    return token;
+}
+
+userSchema.statics.findByCredentials = async(email, password) => {
+    const user = await User.findOne({ email })
+    if (!user)
+        throw new Error('Unable to Login')
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch)
+        throw new Error('Unable to Login');
+
+    return user;
+}
+
 
 userSchema.pre('save', async function(next) {
     const user = this;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const router = new express.Router();
 const User = require('../models/user')
+const auth = require('../middlewares/auth')
 
 router.post('/users', async(req, res) => {
     const user = new User(req.body);
     try {
         await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token })
+    } catch (e) { res.status(400).send(e) }
 })
 
-router.get('/users', async(req, res) => {
+router.post('/users/login', async(req, res) => {
     try {
-        const users = await User.find({})
-        res.status(200).send(users)
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken();
+        res.send({ user, token })
+    } catch (e) { res.status(400).send(e) }
+})
+
+router.post('/users/logout', auth, async(req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => token.token !== req.token)
+        await req.user.save();
+        res.send()
+    } catch (e) { res.status(500).send() }
+})
+
+router.post('/users/logoutall', auth, async(req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save()
+        res.send()
+    } catch (e) { res.status(500).send() }
+})
+
+router.get('/users/me', auth, async(req, res) => {
+    try {
+        res.status(200).send(req.user)
     } catch (e) { res.status(500).send() }
 })
 
@@ -42,7 +65,7 @@ router.patch('/users/:id', async(req, res) => {
         if (!user)
             return res.status(404).send();
         res.send(user)
-    } catch (e) { res.status(500).send(e); }
+    } catch (e) { res.status(500).send(e) }
 })
 
 router.delete('/users/:id', async(req, res) => {
