5009ecec4a786b77a2b93c697e7e27c0e57f3a70
Ali Jawad <ali.jawad@rolustech.net>
2022-05-30 00:52:03 +0500

    User Endpoints Authenticated

--- src/routes/user.js
+++ src/routes/user.js
@@ -42,16 +42,7 @@ router.get('/users/me', auth, async(req, res) => {
     } catch (e) { res.status(500).send() }
 })
 
-router.get('/users/:id', async(req, res) => {
-    try {
-        const user = await User.findById(req.params.id)
-        if (!user)
-            return res.status(404).send();
-        res.send(user);
-    } catch (e) { res.status(500).send(e) };
-})
-
-router.patch('/users/:id', async(req, res) => {
+router.patch('/users/me', auth, async(req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
@@ -59,21 +50,16 @@ router.patch('/users/:id', async(req, res) => {
     if (!isValidOperation)
         return res.status(400).send('Error: Invalid Updates');
     try {
-        const user = await User.findById(req.params.id)
-        Object.assign(user, req.body)
-        await user.save();
-        if (!user)
-            return res.status(404).send();
-        res.send(user)
+        Object.assign(req.user, req.body)
+        await req.user.save();
+        res.send(req.user)
     } catch (e) { res.status(500).send(e) }
 })
 
-router.delete('/users/:id', async(req, res) => {
+router.delete('/users/me', auth, async(req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user)
-            return res.status(404).send();
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) { res.status(500).send() }
 })
 
