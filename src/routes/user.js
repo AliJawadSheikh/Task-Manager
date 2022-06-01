4814412927b307c8d4f80d24cf95bc136912bd3f
Ali Jawad <ali.jawad@rolustech.net>
2022-06-02 02:05:38 +0500

    Storing and Retrieving Avatar from Mongo

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,6 @@ const auth = require('../middlewares/auth')
 const multer = require('multer');
 
 const upload = multer({
-    dest: 'images/avatar',
     limits: {
         fileSize: 1000000
     },
@@ -76,8 +75,26 @@ router.delete('/users/me', auth, async(req, res) => {
     } catch (e) { res.status(500).send() }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async(req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
 }, (err, req, res, next) => res.status(400).send({ error: err.message }))
+
+router.delete('/users/me/avatar', auth, upload.single('avatar'), async(req, res) => {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+})
+
+router.get('/users/:id/avatar', async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user || !user.avatar)
+            throw new Error()
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch (e) { res.status(404).send() }
+})
 
 module.exports = router;
